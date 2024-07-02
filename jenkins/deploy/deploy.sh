0b91a3aa3ae948663d7b7c97824477b673aa6cfb
Phati <phatisavant@gmail.com>
2024-07-02 18:06:39 +0530

    added helm charts

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -13,4 +13,4 @@ docker pull phatijava/$IMAGE:$RELEASE_VERSION
 echo "*** Checking Kubectl access ***"
 
 kubectl get ns
-helm upgrade -f $WORKSPACE/helm-charts/app/values/bill-pay.yaml --set image.tag=$RELEASE_VERSION --install billpay $WORKSPACE/helm-charts/app --namespace cs --create-namespace
+helm upgrade -f $WORKSPACE/helm-charts/app/values/lms.yaml --set image.tag=$RELEASE_VERSION --install lms $WORKSPACE/helm-charts/app --namespace cs --create-namespace
