e960de29eb8ac0dec543d864ddf82142d8cea41d
Phati <phatisavant@gmail.com>
2024-07-06 01:11:44 +0530

    configurable kubernetes endpoint and cert

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -7,4 +7,4 @@ echo "************************************"
 cp target/$TARGET_JAR_NAME_PREFIX-0.0.1-SNAPSHOT.jar jenkins/build/
 
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build
