453e4203cbed802b7d2741e215b7b711e0d98a31
Phati <phatisavant@gmail.com>
2024-07-08 12:32:09 +0530

    made jar file name configurable

--- src/main/java/com/bank/jandhan/lms/controller/LoanApplicationController.java
+++ src/main/java/com/bank/jandhan/lms/controller/LoanApplicationController.java
@@ -1,29 +1,33 @@
 package com.bank.jandhan.lms.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class LoanApplicationController {
 
     @GetMapping("/loan/application")
     public ResponseEntity<?> getLoanApplication(){
+        log.info("loan application api called");
         return new ResponseEntity<>("No applications found", HttpStatus.OK);
     }
 
     @GetMapping("/oom")
     public void oom(){
-//        try {
-//            // Keep allocating memory until OutOfMemoryError
-//            for (int i = 1; ; i++) {
-//                System.out.println("Iteration " + i);
-//                int[] arr = new int[Integer.MAX_VALUE]; // Allocate large array
-//            }
-//        } catch (OutOfMemoryError e) {
-//            System.err.println("Out of Memory!");
-//            e.printStackTrace();
-//        }
+        try {
+            log.info("OOM api called");
+            // Keep allocating memory until OutOfMemoryError
+            for (int i = 1; ; i++) {
+                System.out.println("Iteration " + i);
+                int[] arr = new int[Integer.MAX_VALUE]; // Allocate large array
+            }
+        } catch (OutOfMemoryError e) {
+            System.err.println("****************Out of Memory!***************");
+            e.printStackTrace();
+        }
     }
 }
