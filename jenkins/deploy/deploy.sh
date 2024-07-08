32390d0ac5a5ce57ee004f08c3c328d4188d7f89
Phati <phatisavant@gmail.com>
2024-07-08 11:43:05 +0530

    made jar file name configurable

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -12,4 +12,5 @@ docker pull $DOCKER_HUB_USER/$IMAGE:$RELEASE_VERSION
 
 echo "*** Checking Kubectl access ***"
 kubectl get ns
+helm upgrade --debug --dry-run -f $WORKSPACE/helm-charts/app/values/lms.yaml --set image.repository=$DOCKER_HUB_USER/$IMAGE --set image.tag=$RELEASE_VERSION --install lms $WORKSPACE/helm-charts/app --namespace cs --create-namespace
 helm upgrade -f $WORKSPACE/helm-charts/app/values/lms.yaml --set image.repository=$DOCKER_HUB_USER/$IMAGE --set image.tag=$RELEASE_VERSION --install lms $WORKSPACE/helm-charts/app --namespace cs --create-namespace
--- src/main/java/com/bank/jandhan/lms/controller/LoanApplicationController.java
+++ src/main/java/com/bank/jandhan/lms/controller/LoanApplicationController.java
@@ -13,4 +13,17 @@ public class LoanApplicationController {
         return new ResponseEntity<>("No applications found", HttpStatus.OK);
     }
 
+    @GetMapping("/oom")
+    public void oom(){
+        try {
+            // Keep allocating memory until OutOfMemoryError
+            for (int i = 1; ; i++) {
+                System.out.println("Iteration " + i);
+                int[] arr = new int[Integer.MAX_VALUE]; // Allocate large array
+            }
+        } catch (OutOfMemoryError e) {
+            System.err.println("Out of Memory!");
+            e.printStackTrace();
+        }
+    }
 }
