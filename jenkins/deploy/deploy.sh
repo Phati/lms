b19ad1e8c283a1da66912eae66782b4c23a8a179
Phati <phatisavant@gmail.com>
2024-07-08 10:13:22 +0530

    made jar file name configurable

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -11,4 +11,5 @@ echo "*** Pull docker image image ***"
 docker pull $DOCKER_HUB_USER/$IMAGE:$RELEASE_VERSION
 
 echo "*** Checking Kubectl access ***"
+kubectl get ns
 helm upgrade -f $WORKSPACE/helm-charts/app/values/lms.yaml --set image.repository=$DOCKER_HUB_USER/$IMAGE --set image.tag=$RELEASE_VERSION --install lms $WORKSPACE/helm-charts/app --namespace cs --create-namespace
