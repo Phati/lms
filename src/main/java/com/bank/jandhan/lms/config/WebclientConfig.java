0e1fb1ee4fbc39a63e44bd24239e696f893393bd
Phati <phatisavant@gmail.com>
2024-07-09 14:59:42 +0530

    call external service

--- src/main/java/com/bank/jandhan/lms/config/WebclientConfig.java
+++ src/main/java/com/bank/jandhan/lms/config/WebclientConfig.java
@@ -1,10 +1,15 @@
 package com.bank.jandhan.lms.config;
 
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.PreferHeapByteBufAllocator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebclientConfig {
@@ -13,13 +18,15 @@ public class WebclientConfig {
     String BASE_URL;
     @Bean
     public WebClient webClient() {
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(codecs-> codecs.defaultCodecs().maxInMemorySize(1024 * 1024 ))
+                .build();
+
         return WebClient.builder()
                 .baseUrl(BASE_URL)
-                .exchangeStrategies(
-                        ExchangeStrategies.builder()
-                                .codecs(codecs-> codecs.defaultCodecs().maxInMemorySize(1024 * 1024 ))
-                                .build()
-                )
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create().compress(true)
+                        .option(ChannelOption.ALLOCATOR, new PreferHeapByteBufAllocator(UnpooledByteBufAllocator.DEFAULT))))
+                .exchangeStrategies(exchangeStrategies)
                 .build();
     }
 
