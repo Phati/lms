0c5a56bc744c2166cb1df43edd179e8d5ddf1f13
Phati Sawant <61258100+Phati@users.noreply.github.com>
2025-01-30 03:06:37 +0530

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,4 +8,4 @@ echo "jar name --  $TARGET_JAR_NAME"
 echo "jar version --  $RELEASE_VERSION-$BUILD_NUMBER"
 
 
-docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 -Dbuild.number=${BUILD_NUMBER} -Djar.name=${TARGET_JAR_NAME} -Dapp.version=${RELEASE_VERSION} "$@"
+docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@" -Dbuild.number=${BUILD_NUMBER} -Djar.name=${TARGET_JAR_NAME} -Dapp.version=${RELEASE_VERSION}
