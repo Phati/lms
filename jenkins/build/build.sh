7f5a1aacc9926608111f041d65666b7bb9ecb717
Phati Sawant <61258100+Phati@users.noreply.github.com>
2025-01-30 03:15:10 +0530

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -13,4 +13,4 @@ echo "copying target/$TARGET_JAR_NAME-$RELEASE_VERSION-$BUILD_NUMBER.jar"
 cp target/$TARGET_JAR_NAME-$RELEASE_VERSION-$BUILD_NUMBER.jar jenkins/build/
 
 
-cd jenkins/build/ && docker-compose -f docker-compose-build.yml build
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
