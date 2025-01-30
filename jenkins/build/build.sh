03f90b2e6c31a401afe4ef82608325b58f6b3e8c
Phati Sawant <61258100+Phati@users.noreply.github.com>
2025-01-30 11:44:49 +0530

    Update build.sh

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -12,6 +12,6 @@ rm -rf jenkins/build/*.jar
 echo "copying target/$TARGET_JAR_NAME-$RELEASE_VERSION-$BUILD_NUMBER.jar"
 cp target/$TARGET_JAR_NAME-$RELEASE_VERSION-$BUILD_NUMBER.jar jenkins/build/
 
-docker rmi $(docker images | grep $IMAGE)
+docker images --format "{{.Repository}}:{{.Tag}}" | grep -E '^$IMAGE' | xargs -r docker rmi
 
 cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
